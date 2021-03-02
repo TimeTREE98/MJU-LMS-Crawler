a7663209cc3ccac65fbf839cb250edb866c0b5f5
timetree <timetree@kakao.com>
2021-03-02 12:27:03 +0900

    port change
    default port 3000 > 6000

--- express/app.js
+++ express/app.js
@@ -5,7 +5,7 @@ const logger = require('morgan');
 const moment = require('moment');
 const path = require('path');
 const http = require('http');
-const port = process.env.PORT || '3000';
+const port = process.env.PORT || '6000';
 const app = express();
 
 const mainRouter = require('./routes/main');
